0082e3171ddd1d4d5db4878bb00744476823dc65
Ethan <estjohn2000@gmail.com>
2021-10-09 13:44:47 -0500

    fix name generator function definition

--- Star Squadron/Assets/CharacterGenerator.cs
+++ Star Squadron/Assets/CharacterGenerator.cs
@@ -20,11 +20,8 @@ public class CharacterGenerator : MonoBehaviour
         string[] FirstNameList = new string[13] {"Wally", "Nyssa", "Xena", "Varina", "Geneva", "Astrid", "Fae", "Phoebe", "Ethan", "Alan", "Shae", "Harper", "Jace"};
         string[] TitleList = new string[7] {"the Destroyer", "the Wise", "the III", "the Blind", "the Friend", "the Bitch ASS", "the Lesser"};
 
-        string FirstName;
-
-        FirstName = FirstNameList[Random.Range(0, FirstNameList.Length)];
-        NameTitle = TitleList[Random.Range(0, TitleList.Length)];
-
+        string FirstName = FirstNameList[Random.Range(0, FirstNameList.Length)];
+        string NameTitle = TitleList[Random.Range(0, TitleList.Length)];
         string mercenaryName = FirstName + " " + NameTitle;
 
         Debug.Log(mercenaryName);
