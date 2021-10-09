207ae9cb14bd0e2aa499ecba46627145f6e499c7
Ethan <estjohn2000@gmail.com>
2021-10-09 13:38:15 -0500

    fixed name array numbers

---
+++ Star Squadron/Assets/CharacterGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GenerateName()
+    {
+        string[] FirstNameList = new string[13] {"Wally", "Nyssa", "Xena", "Varina", "Geneva", "Astrid", "Fae", "Phoebe", "Ethan", "Alan", "Shae", "Harper", "Jace"};
+        string[] TitleList = new string[7] {"the Destroyer", "the Wise", "the III", "the Blind", "the Friend", "the Bitch ASS", "the Lesser"};
+
+        string FirstName;
+
+        FirstName = FirstNameList[Random.Range(0, FirstNameList.Length)];
+        NameTitle = TitleList[Random.Range(0, TitleList.Length)];
+
+        string mercenaryName = FirstName + " " + NameTitle;
+
+        Debug.Log(mercenaryName);
+    }
+}
